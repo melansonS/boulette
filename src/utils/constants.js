0c47b80af19a889f45a0e4e02653c4b97b458eec
Samuel Melanson <samuelmelanson@live.com>
2021-02-12 22:30:40 -0500

    server url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-export const URL = "http://localhost:4001";
-// export const URL = "https://boulette-server.herokuapp.com";
+// export const URL = "http://localhost:4001";
+export const URL = "https://boulette-server.herokuapp.com";
